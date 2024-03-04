88ad0947f4c0012a6a4e4f15e3b5b01cef2dbde7
itsmasad <muhammadasadofficial@gmail.com>
2024-03-04 18:48:37 +0500

    updated restassured code

---
+++ restassured/src/main/java/API/HTTPRequests.java
@@ -0,0 +1,21 @@
+package API;
+import org.testng.annotations.Test;
+/*
+given()
+    Content type, set cookies, add auth, add params, set headers info etc
+when()
+    Get, Post, Put, Delete
+then()
+    Validate status code, extract response, extract headers cookies & response body. . . .
+ */
+
+public class HTTPRequests {
+
+    @Test
+    void getUser()
+    {
+
+    }
+
+    
+}
